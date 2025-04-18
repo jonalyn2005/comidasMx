6689714f49627d67ea9bee33a3d3aa5f5d81851b
jonalyn2005 <jonathan.moreno.17.20@gmail.com>
2025-04-18 01:21:10 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+// Función para abrir y cerrar el menú
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("show-menu");
+}
+
+// Función para cerrar el menú al hacer clic en un enlace
+function closeMenu() {
+    document.getElementById("menu").classList.remove("show-menu");
+}
